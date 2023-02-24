1f142746bb5cf00a3e1f47659df48a1921888d44
Cameron Lares <cameronlares@gmail.com>
2023-02-24 11:00:24 -0800

    App.js cleanup

--- App.js
+++ App.js
@@ -1,60 +1,59 @@
-import { Text, View, Alert } from 'react-native'
+import { View, Alert } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import styles from './App.styles'
-import ImageOption from './src/components/ImageOption/'
-// import question from './assets/data/imageMulatipleChoiceQuestions'
-import Button from './src/components/Button'
 import ImageMultipleChoiceQuestions from './src/components/ImageMultipleChoiceQuestions/ImageMultipleChoiceQuestions'
 import OpenEndedQuestion from './src/components/OpenEndedQuestion/OpenEndedQuestion'
 import question from './assets/data/allQuestions'
 
-
 const App = () => {
-
+  //Set Index, and set 
   const [currentQuestionIndex, SetCurrentQuestionIndex] = useState(1)
-  const [currentQuestion, SetCurrentQuestion] = useState(question[currentQuestionIndex])
+  const [currentQuestion, SetCurrentQuestion] = useState(question[currentQuestionIndex]
+  )
 
   useEffect(() => {
     //When index is out of bounce, you won appears and it restarts by setting index state to 0
     if (currentQuestionIndex >= question.length) {
-      Alert.alert("You won");
-      SetCurrentQuestionIndex(0);
+      Alert.alert('You won')
+      SetCurrentQuestionIndex(1)
     } else {
       console.log(`Use Effect Called For Index:${currentQuestionIndex}`)
-      SetCurrentQuestion(question[currentQuestionIndex]);
+      SetCurrentQuestion(question[currentQuestionIndex])
     }
-
   }, [currentQuestionIndex])
 
-  //Next Index
   const onCorrect = () => {
+    // Update Index
     SetCurrentQuestionIndex(currentQuestionIndex + 1)
   }
 
   const onWrong = () => {
-    Alert.alert("Wrong")
+    Alert.alert('Wrong')
   }
+
+
+
+
+
   return (
     <View style={styles.root}>
       {/* //If first side of && operator is false, the result */}
-      {currentQuestion.type === 'IMAGE_MULTIPLE_CHOICE' &&
+      {currentQuestion.type === 'IMAGE_MULTIPLE_CHOICE' && (
         <ImageMultipleChoiceQuestions
           question={currentQuestion}
           onCorrect={onCorrect}
           onWrong={onWrong}
         />
-      }
-
-      {
-        currentQuestion.type === 'OPEN_ENDED' &&
-          <OpenEndedQuestion
-            question={currentQuestion}
-            onCorrect={onCorrect}
-            onWrong={onWrong}
-          />
-      }
-
-    </View>)
+      )}
+      {currentQuestion.type === 'OPEN_ENDED' && (
+        <OpenEndedQuestion
+          question={currentQuestion}
+          onCorrect={onCorrect}
+          onWrong={onWrong}
+        />
+      )}
+    </View>
+  )
 }
 
 export default App

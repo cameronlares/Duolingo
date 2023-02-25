b4c1e60466c82f9d05e3440882d9c6fcb3ce8b52
Cameron Lares <cameronlares@gmail.com>
2023-02-25 13:44:04 -0800

    Added Async Storage React Native package to save and load data where user left off

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { View, Alert } from 'react-native'
+import { View, Alert, ActivityIndicator } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import styles from './App.styles'
 import ImageMultipleChoiceQuestions from './src/components/ImageMultipleChoiceQuestions/ImageMultipleChoiceQuestions'
 import OpenEndedQuestion from './src/components/OpenEndedQuestion/OpenEndedQuestion'
 import question from './assets/data/allQuestions'
 import Header from './src/components/Header/Header'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const App = () => {
   //Set Index, and set 
@@ -12,6 +14,7 @@ const App = () => {
   const [currentQuestion, SetCurrentQuestion] = useState(question[currentQuestionIndex]
   )
 const [lives, setLives] = useState(2)
+const [hasLoaded, setHasLoaded] = useState(false)
 
   useEffect(() => {
     //When index is out of bounce, you won appears and it restarts by setting index state to 0
@@ -22,6 +25,21 @@ const [lives, setLives] = useState(2)
       SetCurrentQuestion(question[currentQuestionIndex])
     }
   }, [currentQuestionIndex])
+
+
+  // Loads when component mounts
+useEffect(()=>{
+  
+loadData();
+},[])
+
+// Save everytime lives and index are updated
+useEffect(()=>{
+  //Only Save is hasLoaded is true
+  if(hasLoaded){
+    saveData()
+  }
+},[lives, currentQuestionIndex, hasLoaded]);
 
   const onCorrect = () => {
     // Update Index
@@ -46,7 +64,33 @@ const onWrong = () => {
     else{
       Alert.alert('Wrong')
       setLives(lives-1)
+     
     }}
+
+const saveData = async () => {
+await AsyncStorage.setItem('lives', lives.toString())
+await AsyncStorage.setItem('currentQuestionIndex', currentQuestionIndex.toString())
+
+}
+
+const loadData = async () => {
+const loadedLives = await AsyncStorage.getItem('lives')
+if(loadedLives){
+  setLives(parseInt(loadedLives));
+}
+const currentQuestionIndex = await AsyncStorage.getItem('currentQuestionIndex')
+if(SetCurrentQuestionIndex){
+  SetCurrentQuestionIndex(parseInt(currentQuestionIndex));
+}
+
+//Loaded is true
+setHasLoaded(true)
+}
+
+//Because hasLoaded isn't done, it will not reach the second return object until hasLoaded is set to true
+if(!hasLoaded){
+ return <ActivityIndicator  size="large" color="#00ff00"/>
+}
 
   return (
     <View style={styles.root}>
